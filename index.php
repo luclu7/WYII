686a05774fd2e8edc334f11a015e1f36d9558ec4
Luclu7gaming <theluc7andcompagnie@gmail.Com>
2016-07-31 15:36:59 +0200

    First file
    Added index.php, the main file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>WYIT?</title>
+</head>
+<body>
+
+<p>
+    We are in <?php date_default_timezone_set('Europe/London'); echo date("Y"); ?>.
+</p>
+
+</body>
+</html>
