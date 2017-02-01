b88ffbc1bb769a6313bf24e0af06f3199e2774bd
Luclu7 <luclu7@gmx.fr>
2017-02-01 17:03:17 +0100

    Added autoreload and Bootstrap

--- index.php
+++ index.php
@@ -2,16 +2,23 @@
 <html>
 <head>
     <meta http-equiv="refresh" content="5; URL=https://whatyearisit.date/">
+    
     <meta charset="utf-8">
+    
     <link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8" />
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
     <title>WYIT?</title>
 </head>
 <body>
 
 <div class="centered">
-<h1>
+<h1 class=text-center>
     We are in <?php echo date("Y"); ?>.
 </h1>
 
 </body>
 </html>
+<!-- Hello :D -->
