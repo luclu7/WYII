d272da3b294c3b105e17b5d439952443205995c3
leonekmi <leonekmi@hotmail.com>
2017-02-21 18:08:17 +0100

    Arguments
    ?timezone=
    https://secure.php.net/manual/fr/timezones.php

--- api.php
+++ api.php
@@ -1,6 +1,10 @@
 <?php
 
-date_default_timezone_set('Europe/Paris');
+if (isset($_GET['timezone'])) {
+  date_default_timezone_set($_GET['timezone']);
+} else {
+  date_default_timezone_set('Europe/Paris');
+}
 $api = date("Y");
 $result = array("year" => $api);
 $json = json_encode($result);
